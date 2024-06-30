8011144db4b07ad0ac1cd76871f5eee45eaea7d7
Lautaro Intraub <lauti.intraub@gmail.com>
2024-06-30 19:54:31 -0300

    Subida del proyecto

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const task_form = document.querySelector('#add-task');
+const todos = document.querySelector('#todos');
+
+let task_counter = 0;
+
+function createTask(task) {
+    const task_container = document.createElement('article');
+    task_container.className = 'task';
+    task_counter++;
+    task_container.id = `task-${task_counter}`;
+
+    task_container.innerHTML = `
+    <div>
+      <h3>Número de cheque: ${task.description}</h3>
+      <h4>Banco: ${task.banco}</h4>
+      <p>Plazo: ${task.deadline}</p>
+    </div>
+    <p class="${task.status.toLowerCase()}">${task.status}</p>
+    <button id="btn-${task_counter}" class="btn-delete" type="button">Eliminar</button>
+  `;
+
+    todos.appendChild(task_container);
+
+    const deleteButton = document.querySelector(`#btn-${task_counter}`);
+    deleteButton.addEventListener('click', (e) => {
+        const id = e.target.id.split('-')[1];
+        document.querySelector(`#task-${id}`).remove();
+    })
+};
+
+
+
+task_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const task = {
+        description: e.target[0].value,
+        banco: e.target[1].value,
+        deadline: e.target[2].value,
+        status: e.target[3].value
+    };
+
+    createTask(task);
+
+    task_form.reset();
+});
